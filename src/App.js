f8be5eeb2d73c2cc9fa2a4c60d303a4e2e7418f1
silijove <42864526+silijove@users.noreply.github.com>
2018-12-17 10:20:31 +0100

    Dependencies fix

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const Home = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+
+const Topic = ({ match }) => (
+  <h3>
+    <span>
+      Requested Param:
+      {match.params.id}
+    </span>
+  </h3>
+);
+Topic.propTypes = {
+  match: PropTypes.func.isRequired,
+};
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+
+    <ul>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:id`} component={Topic} />
+    <Route
+      exact
+      path={match.path}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
+Topics.propTypes = {
+  match: PropTypes.func.isRequired,
+};
+
+const Header = () => (
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/about">About</Link>
+    </li>
+    <li>
+      <Link to="/topics">Topics</Link>
+    </li>
+  </ul>
+);
+
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  </Router>
+);
 
 export default App;
